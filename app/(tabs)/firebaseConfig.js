2fbee34e66fa956c4a68c8faaa9416cb94410135
jmmartinezr <jmmr0049@red.ujaen.es>
2025-03-13 10:17:40 +0100

    Index conectado a firebase

--- app/(tabs)/firebaseConfig.js
+++ app/(tabs)/firebaseConfig.js
@@ -2,16 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyDxJPAcTuO_BO2339OnoJHUYS49YzvsA4E",
+  authDomain: "omnisense-1d992.firebaseapp.com",
+  projectId: "omnisense-1d992",
+  storageBucket: "omnisense-1d992.firebasestorage.app",
+  messagingSenderId: "930179210850",
+  appId: "1:930179210850:web:db53a9592081bb8f773b3b"
 };
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+console.log("Firebase configurado correctamente:", app.name);  // Esto debería mostrar "Firebase" si todo está bien.
+
 
 export { db, addDoc, collection };
