f9e55791922dfa88bc8ce3ffcdb43c435b893e9b
jmmartinezr <jmmr0049@red.ujaen.es>
2025-03-16 11:49:02 +0100

    Configuración firebase corregida

--- app/(tabs)/firebaseConfig.js
+++ app/(tabs)/firebaseConfig.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDxJPAcTuO_BO2339OnoJHUYS49YzvsA4E",
   authDomain: "omnisense-1d992.firebaseapp.com",
   projectId: "omnisense-1d992",
-  storageBucket: "omnisense-1d992.firebasestorage.app",
+  storageBucket: "omnisense-1d992.appspot.com",
   messagingSenderId: "930179210850",
   appId: "1:930179210850:web:db53a9592081bb8f773b3b"
 };
