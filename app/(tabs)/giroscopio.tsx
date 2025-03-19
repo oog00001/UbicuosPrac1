21049aa9a1c5bb883cb883c898fe80f06deae30e
jmmartinezr <jmmr0049@red.ujaen.es>
2025-03-19 11:30:49 +0100

    Fecha en histórico

--- app/(tabs)/giroscopio.tsx
+++ app/(tabs)/giroscopio.tsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect, useRef, useLayoutEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ScrollView, Button } from 'react-native';
 import { LineChart } from 'react-native-chart-kit';
-import { Gyroscope } from 'expo-sensors';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import useSensors from '../../hooks/useSensors';
+import { db, collection } from './firebaseConfig';
+import { onSnapshot, query, orderBy } from "firebase/firestore";
 
 interface GyroscopioData {
     x: number;
@@ -12,45 +14,68 @@ interface GyroscopioData {
 }
 
 export default function Giroscopio() {
-    const [gyroData, setGyroData] = useState({ x: 0, y: 0, z: 0 });
+    const { gyroData } = useSensors();
     const [gyroHistory, setGyroHistory] = useState<GyroscopioData[]>(
         Array.from({ length: 20 }, () => ({ x: 0, y: 0, z: 0 }))
     );
     const [containerWidth, setContainerWidth] = useState<number>(0);
-    const gyroSubscriptionRef = useRef<any>(null);
     const navigation = useNavigation();
+
+    const [firebaseData, setFirebaseData] = useState<GyroscopioData[]>([]);
+    const [displayedData, setDisplayedData] = useState<GyroscopioData[]>([]);
+    const [currentIndex, setCurrentIndex] = useState(20);
 
     useEffect(() => {
         navigation.setOptions({
-            title: "Giroscopio",
+            title: 'Giroscopio',
             headerLeft: () => (
                 <FontAwesome5
-                    name="arrow-left"
+                    name='arrow-left'
                     size={20}
-                    color="white"
+                    color='white'
                     style={{ marginLeft: 20, marginRight: 30 }}
                     onPress={() => navigation.goBack()}
                 />
             ),
         });
 
-        Gyroscope.setUpdateInterval(500);
-        gyroSubscriptionRef.current = Gyroscope.addListener((data: GyroscopioData) => {
-            setGyroData(data);
-            setGyroHistory(prevHistory => {
-                if (!isFinite(data.x) || !isFinite(data.y) || !isFinite(data.z)) return prevHistory;
-                const updatedHistory = [...prevHistory, data];
-                return updatedHistory.length > 20 ? updatedHistory.slice(-20) : updatedHistory;
-            });
+
+        setGyroHistory(prevHistory => {
+            if (!isFinite(gyroData.x) || !isFinite(gyroData.y) || !isFinite(gyroData.z)) return prevHistory;
+            const updatedHistory = [...prevHistory, gyroData];
+            return updatedHistory.length > 20 ? updatedHistory.slice(-20) : updatedHistory;
         });
 
-        return () => {
-            if (gyroSubscriptionRef.current) gyroSubscriptionRef.current.remove();
-        };
-    }, [navigation]);
+    }, [navigation, gyroData]);
+
+    useEffect(() => {
+        const accelCollection = collection(db, 'giroscopio');
+        const accelQuery = query(accelCollection, orderBy('timestamp', 'asc'));
+
+        const unsubscribe = onSnapshot(accelQuery, (snapshot) => {
+            const data = snapshot.docs.map(doc => doc.data() as GyroscopioData);
+            setFirebaseData(data);
+
+            setDisplayedData((prevDisplayedData) =>
+                prevDisplayedData.length > 20 ? prevDisplayedData : data.slice(0, 20)
+            );
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    const loadMoreData = () => {
+        const nextIndex = currentIndex + 20;
+        const newData = firebaseData.slice(0, nextIndex);
+
+        if (newData.length > displayedData.length) {
+            setDisplayedData(newData);
+            setCurrentIndex(nextIndex);
+        }
+    };
 
     return (
-        <View style={styles.screen}>
+        <ScrollView style={styles.screen} keyboardShouldPersistTaps='handled' contentContainerStyle={{ paddingBottom: 30 }}>
             <View
                 style={styles.container}
                 onLayout={(event) => {
@@ -95,8 +120,24 @@ export default function Giroscopio() {
                     />
                 )}
                 <Text style={styles.historyText}>Histórico:</Text>
+                <View>
+                    {displayedData.map((item, index) => (
+                        <View key={index} style={styles.row}>
+                            <Text style={styles.cell}> {item.timestamp}</Text>
+                            <Text style={styles.cell}>X: {item.x.toFixed(3)}</Text>
+                            <Text style={styles.cell}>Y: {item.y.toFixed(3)}</Text>
+                            <Text style={styles.cell}>Z: {item.z.toFixed(3)}</Text>
+                        </View>
+                    ))}
+
+                    {firebaseData.length > displayedData.length && (
+                        <View style={{ marginTop: 10, marginBottom: 20 }}>
+                            <Button title='Cargar más' onPress={loadMoreData} />
+                        </View>
+                    )}
+                </View>
             </View>
-        </View>
+        </ScrollView>
     );
 }
 
@@ -137,5 +178,17 @@ const styles = StyleSheet.create({
         fontSize: 18,
         marginBottom: 15,
         fontWeight: 'bold',
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ddd',
+    },
+    cell: {
+        flex: 1,
+        textAlign: 'center',
+        fontSize: 16,
     },
 });
